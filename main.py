679e931d73f267063bd08de3bfcc2d9bae791105
muhzinkhan <muhzin029@gmail.com>
2023-10-03 23:44:24 +0530

    Update button functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import customtkinter
-import tkinter as tk
-from tkinter import Canvas
-from PIL import Image, ImageTk
+from tkinter import messagebox, END
+import random
+import pyperclip
+import json
+from PIL import Image
 
 
 DEFAULT_EMAIL = ""
@@ -9,16 +11,105 @@ FONT_large = ("Microsoft YaHei UI", 17)
 FONT_normal = ("Microsoft YaHei UI", 13)
 
 
-def save():
-    pass
-
-
-def search():
-    pass
-
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_length = 16
+    random_3_set = [random.randint(2, 7) for _ in range(2)]
+    random_3_set.append(password_length - sum(random_3_set))
+
+    nr_letters = random_3_set[0]
+    nr_symbols = random_3_set[1]
+    nr_numbers = random_3_set[2]
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+
+    pyperclip.copy(password)
+
+    # password_copied_label
+    password_copied = customtkinter.CTkLabel(app, text="Password copied to clipboard!", font=FONT_normal,
+                                             fg_color="transparent")
+    password_copied.grid(row=5, column=0, columnspan=3, padx=1, pady=1, sticky="EW")
+    password_copied.after(5000, password_copied.destroy)
+
+
+# ---------------------------- SEARCH SAVED PASSWORD ------------------------------- #
+
+def search():
+    website = website_entry.get()
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Please enter a website to search.")
+    else:
+        try:
+            with open("data/data.json") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found.")
+        else:
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                pyperclip.copy(password)
+                messagebox.showinfo(title=website, message=f"Email:          {email}\nPassword:  {password}\n\nPassword has been copied to the clipboard!")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for '{website}' exists.")
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"Email:          {email}\nPassword:  {password} \n\nIs it okay to save?")
+        if is_ok:
+            try:
+                with open("data/data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data/data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+
+                with open("data/data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI S1 SETUP ------------------------------- #
@@ -27,6 +118,7 @@ app = customtkinter.CTk()
 # app.geometry("720x480")
 app.title("App Manager")
 app.config(padx=50, pady=50)
+
 
 # -------Center Logo-------
 
@@ -50,8 +142,7 @@ password_label = customtkinter.CTkLabel(app, text="Password", font=FONT_normal, 
 password_label.grid(row=3, column=0, padx=3, pady=3)
 
 # password_copied_label
-password_copied = customtkinter.CTkLabel(app, text="Password copied to clipboard!", font=FONT_normal, fg_color="transparent")
-password_copied.grid(row=5, column=0, columnspan=3, padx=1, pady=1, sticky="EW")
+# Inside the generate_password() definition
 
 
 # -------Entries -------
